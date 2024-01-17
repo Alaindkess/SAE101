2d29f9bde5efc830fc7b8998d263625727a8853f
Alaindkess <94915565+Alaindkess@users.noreply.github.com>
2024-01-17 14:39:31 +0100

    ajout base

---
+++ asset/header/header.php
@@ -0,0 +1,16 @@
+
+
+<header>
+    <div id="auteur">
+        <p>Alain DUPONT</p>
+        <p>Robin LETRANGE</p>
+    </div>
+    
+    <div id="titre">
+        <p>Rapport de la SAE 101 Automatisme</p>
+    </div>
+
+    <div id="logo">
+        <img src="asset/img/logo_unistra.png" alt="logo de l\'universite" size="60px">
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rapport SAE 101</title>
+    <meta author="Alain DUPONT Robin LETRANGE">
+
+    <link rel="stylesheet" href="asset/style.css">
+    <link rel="stylesheet" href="asset/header/header.css">
+    <link rel="stylesheet" href="asset/footer/footer.css">
+
+</head>
+
+
+
+
+<body>
+
+    <?php require_once 'asset/header/header.php'?> 
+
+    <article>
+        <section>
+            <h1>L'analyse du probleme</h1>
+        </section>
+    </article>
+
+    <?php require_once 'asset/footer/footer.php'?> 
+
+</body>
+</html>
